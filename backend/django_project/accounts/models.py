66a84aa9afb5201e521a1a62f41f780c72319fa5
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-12-21 11:48:20 +0900

    accoutns-signup test.

--- backend/django_project/accounts/models.py
+++ backend/django_project/accounts/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import date
+import re
+from django.core.exceptions import ValidationError
+
+
+def validate_password(value):
+    if not re.match('^(?=.*[a-zA-Z0-9]).{6,}$', value):
+        raise ValidationError('Password must be at least 6 characters and contain only letters and numbers.')
+
+
 
 class CustomUser(AbstractUser):
     """Extension User Model"""
     
     # name,birthday,sex は必須
     name=models.CharField(default='unkown',null=False,max_length=100)
+    password = models.CharField(
+        max_length=128,
+        null=False,
+        blank=False,
+        validators=[validate_password]
+    )
+    
     birthday = models.DateField(default=date.today)
     sex = models.BooleanField(default=False) # 0 is male, 1 is female.
     
--- backend/django_project/accounts/tests.py
+++ backend/django_project/accounts/tests.py
@@ -1,3 +1,126 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from accounts.models import CustomUser  # このimport文を適切なモジュールに置き換えてください
+from accounts.serializers import CustomUserSerializer  # このimport文を適切なモジュールに置き換えてください
+from datetime import date
 
-# Create your tests here.
+ENDPOINT = 'http://127.0.0.1:8000/'
+
+class SignupAPIViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.signup_url = ENDPOINT+'accounts/signup/'  # このURLは実際のエンドポイントに置き換えてください
+
+    def create_test_user(self, email_check=False):
+        # テストで使用するデータを作成
+        user_data = {
+            'username': 'testuser',
+            'password': 'securepassword',
+            'email': 'testuser@example.com',
+            'name': 'John Doe',
+            'birthday': '2000-01-01',  # 適切な形式に変更すること
+            'sex': False,
+            'email_check': False,
+        }
+
+        # POSTリクエストを実行
+        response = self.client.post(self.signup_url, user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('detail', response.data)
+        
+
+    def test_signup_existing_user_email_check_true(self):
+        # 既存のユーザーを作成
+        existing_user_data = {
+            'username': 'test@example.com',
+            'password': 'securepassword',
+            'email': 'test@example.com',
+            'name': 'John Doe',
+            'birthday': date.today(),
+            'sex': False,
+            'email_check': True,
+        }
+        self.create_test_user(existing_user_data)
+        
+        new_user_data = {
+            'username': 'test@example.com',
+            'password': 'newpassword',
+            'email': 'test@example.com',  # 既存ユーザーと同じemailに変更
+            'name': 'John Doe',
+            'birthday': date.today(),
+            'sex': False,
+            'email_check': True,  # 既存ユーザーが削除されるため新しいユーザーは作成される
+        }
+
+        response = self.client.post(self.signup_url, new_user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('Success! Check your email for confirmation.', response.data['detail'])
+
+
+    def test_signup_existing_user_email_check_false(self):
+        # 既存のユーザーを作成
+        existing_user_data = {
+            'username': 'test@example.com',
+            'password': 'securepassword',
+            'email': 'test@example.com',
+            'name': 'John Doe',
+            'birthday': date.today(),
+            'sex': False,
+            'email_check': False,
+        }
+        self.create_test_user(existing_user_data)
+        
+        new_user_data = {
+            'username': 'test@example.com',
+            'password': 'newpassword',
+            'email': 'test@example.com',  # 既存ユーザーと同じemailに変更
+            'name': 'John Doe',
+            'birthday': date.today(),
+            'sex': False,
+            'email_check': False,  # 既存ユーザーが削除されるため新しいユーザーは作成される
+        }
+
+        response = self.client.post(self.signup_url, new_user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('Success! Check your email for confirmation.', response.data['detail'])
+    
+
+    # def test_signup_invalid_data(self):
+    #     # 無効なデータでテスト
+    #     data = {
+    #         # 'email': 'invalidemail',  # 無効なメールアドレス
+    #         'password': 'short',  # 短すぎるパスワード
+    #     }
+
+    #     response = self.client.post(self.signup_url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #     # self.assertIn('This field must be a valid email address.', response.data['email'][0])
+    #     self.assertIn('Ensure this field has at least 8 characters.', response.data['password'][0])
+        
+    # def test_signup_empty_username(self):
+    #     data = {
+    #         'email': 'test@example.com',
+    #         'password': 'securepassword',
+    #         'username': '',  # 空のusername
+    #     }
+
+    #     response = self.client.post(self.signup_url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #     self.assertIn('This field may not be blank.', response.data['username'][0])
+
+    # def test_signup_empty_password(self):
+    #     data = {
+    #         'email': 'test@example.com',
+    #         'password': '',  # 空のpassword
+    #         'username': 'testuser',
+    #     }
+
+    #     response = self.client.post(self.signup_url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    #     self.assertIn('This field may not be blank.', response.data['password'][0])
+
+
+
+    
