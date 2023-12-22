6609e75f185fff9e9ec62b146f7b418e2367ecb7
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-12-22 13:17:33 +0900

    exercise get-exercise test.

--- backend/django_project/exercise/tests.py
+++ backend/django_project/exercise/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
-from .models import Workout  
+from .models import Workout,Exercise
 from .serializers import WorkoutSerializer  
 from accounts.models import CustomUser
 from rest_framework.authtoken.models import Token
 from .default_workout import default_workout_data
+from django.utils import timezone
+from datetime import timedelta
 
 
 ENDPOINT = 'http://127.0.0.1:8000/exercise/'
@@ -103,3 +105,60 @@ class WorkoutCreateViewTest(APITestCase):
         
 
 
+
+
+# 3. Exercise Get By Date
+class ExerciseGetByDateViewTest(APITestCase):
+    def setUp(self):
+        # テストユーザーの作成
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'securepassword',
+            'email': 'testuser@example.com',
+            'name': 'John Doe',
+            'birthday': '2000-01-01',
+            'sex': False,
+            'email_check': True,
+        }
+        self.user = CustomUser.objects.create_user(**self.user_data)
+        # ログイン
+        self.client.login(username='testuser', password='securepassword')
+        # トークンの取得
+        self.token, _ = Token.objects.get_or_create(user=self.user)
+        # トークンをセット
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.get_workout_url = ENDPOINT+'get-workout/'
+        # テスト用のWorkoutの作成
+        self.workout = Workout.objects.create(account=self.user, is_default=False, name='Test Workout', workout_type='Other')
+        
+        # exercise get url
+        self.get_exercise_url = ENDPOINT+'get-exercise-date/'
+        
+        # テスト用のデータ
+        self.exercise_date = '2023-12-21'
+
+    def test_get_exercise_by_date_success(self):
+        # Exerciseを作成
+        exercise_date = timezone.now().date()
+        exercise = Exercise.objects.create(
+            account=self.user,
+            exercise_date=exercise_date,
+            workout=self.workout,
+            sets=3,
+            reps=10
+        )
+
+        # GETリクエストを送信
+        response = self.client.get(self.get_exercise_url, {'date': self.exercise_date}, format='json')
+
+        # レスポンスの確認
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # 必要に応じて、返ってくるデータの構造や内容をテスト
+
+    def test_get_exercise_by_date_failure_missing_date(self):
+        # 'date'パラメータがない場合のGETリクエストを送信
+        response = self.client.get(self.get_exercise_url, format='json')
+
+        # レスポンスの確認
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # 必要に応じて、返ってくるエラーメッセージなどをテスト
